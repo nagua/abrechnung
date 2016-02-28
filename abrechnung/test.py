5fc73ae66f97c6fd94820c5b4a30afd2fcb9bda4
Hendrik Woidt <woidt@kth.se>
2016-02-28 10:54:39 +0100

    Use unittest module and wrap the test into a test case class

--- abrechnung/test.py
+++ abrechnung/test.py
@@ -1,54 +1,61 @@
 #!/usr/bin/env python3
 # vim: tabstop=2 expandtab shiftwidth=2 softtabstop=2
 
+import unittest
 import event as e
 import account as a
 import group as g
 
 def gen_test_group():
-  """
-  -----------------------------------------------------------------------------------------
-  This is the test function you want to execute!!!!
-  -----------------------------------------------------------------------------------------
-  """
-  group = g.Group(0)
-  
-  # Add accounts
-  group.add_account(a.Account("nicolas"))
-  group.add_account(a.Account("max"))
-  group.add_account(a.Account("sandrina"))
-  group.add_account(a.Account("annika"))
-  
-  # Add events
-  group.add_event(e.Event(30, "max", ["nicolas", "max", "sandrina"]))
-  group.print_account_data()
-  group.check_balance()
-  print()
+  """ some function for backwards compatibility """
+  t = TestAccounting()
+  return t.test_group_balancing()
 
-  group.add_event(e.Event(20, "max", ["nicolas", "max", "sandrina"]))
-  group.print_account_data()
-  group.check_balance()
-  print()
+class TestAccounting(unittest.TestCase):
+  def test_group_balancing(self):
+    """
+    --------------------------------------------------------------------------
+    This is the test function you want to execute!!!!
+    --------------------------------------------------------------------------
+    """
+    group = g.Group(0)
+    
+    # Add accounts
+    group.add_account(a.Account("nicolas"))
+    group.add_account(a.Account("max"))
+    group.add_account(a.Account("sandrina"))
+    group.add_account(a.Account("annika"))
+    
+    # Add events
+    group.add_event(e.Event(30, "max", ["nicolas", "max", "sandrina"]))
+    group.print_account_data()
+    group.check_balance()
+    print()
 
-  group.add_event(e.Event(50, "max", ["nicolas", "max", "sandrina", "annika"]))
-  group.print_account_data()
-  group.check_balance()
-  print()
+    group.add_event(e.Event(20, "max", ["nicolas", "max", "sandrina"]))
+    group.print_account_data()
+    group.check_balance()
+    print()
 
-  group.add_event(e.Event(30, "nicolas", ["nicolas", "max", "annika"]))
-  group.print_account_data()
-  group.check_balance()
-  print()
+    group.add_event(e.Event(50, "max", ["nicolas", "max", "sandrina", "annika"]))
+    group.print_account_data()
+    group.check_balance()
+    print()
 
-  group.add_event(e.Event(60.50, "nicolas", ["nicolas", "max", "sandrina"]))
-  group.print_account_data()
-  group.check_balance()
-  print()
+    group.add_event(e.Event(30, "nicolas", ["nicolas", "max", "annika"]))
+    group.print_account_data()
+    group.check_balance()
+    print()
 
-  # Calculate transactions needed to balance accounts
-  group.calculate_balancing()
+    group.add_event(e.Event(60.50, "nicolas", ["nicolas", "max", "sandrina"]))
+    group.print_account_data()
+    group.check_balance()
+    print()
 
-  return group
+    # Calculate transactions needed to balance accounts
+    group.calculate_balancing()
+
+    return group
 
 if __name__ == "__main__":
-  gen_test_group()
+  unittest.main()
