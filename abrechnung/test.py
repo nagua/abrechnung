c7009fa03de7cbefe35686204d23b87a10e366c9
Hendrik Woidt <woidt@kth.se>
2016-02-28 11:02:52 +0100

    Factor out group setup into setUp() function

--- abrechnung/test.py
+++ abrechnung/test.py
@@ -9,15 +9,14 @@ import group as g
 def gen_test_group():
   """ some function for backwards compatibility """
   t = TestAccounting()
-  return t.test_group_balancing()
+  t.run()
+  return t.group
 
 class TestAccounting(unittest.TestCase):
-  def test_group_balancing(self):
-    """
-    --------------------------------------------------------------------------
-    This is the test function you want to execute!!!!
-    --------------------------------------------------------------------------
-    """
+  def tearDown(self):
+    pass
+
+  def setUp(self):
     group = g.Group(0)
     
     # Add accounts
@@ -25,7 +24,17 @@ class TestAccounting(unittest.TestCase):
     group.add_account(a.Account("max"))
     group.add_account(a.Account("sandrina"))
     group.add_account(a.Account("annika"))
-    
+
+    self.group = group
+
+  def test_group_balancing(self):
+    """
+    --------------------------------------------------------------------------
+    This is the test function you want to execute!!!!
+    --------------------------------------------------------------------------
+    """
+    group = self.group
+
     # Add events
     group.add_event(e.Event(30, "max", ["nicolas", "max", "sandrina"]))
     group.print_account_data()
