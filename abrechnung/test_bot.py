559692371ab337fbf895cd7a3f14084d69240a3a
Hendrik Woidt <woidt@kth.se>
2017-05-27 10:24:46 +0200

    Fix unittests to incorporate billingdata and backup_file

--- abrechnung/test_bot.py
+++ abrechnung/test_bot.py
@@ -1,13 +1,16 @@
 import unittest
 from unittest.mock import Mock
 from abrechnungsbot import AbrechnungsBot
+from billingdata import BillingData
+from group import Group
 
 class TestAbrechnungsBot(unittest.TestCase):
     def setUp(self):
         self.groupid = 123
-        self.groups = {}
-        self.botcfg = {"private_chat":self.groupid, "token":"Pa$$w0rd"}
-        self.bot = AbrechnungsBot(self.botcfg, self.groups)
+        self.billingdata = BillingData()
+
+        self.botcfg = {"private_chat":self.groupid, "token":"Pa$$w0rd", "backup_file":None}
+        self.bot = AbrechnungsBot(self.botcfg, self.billingdata)
 
         self.update_mock = Mock()
         self.update_mock.message.chat_id = self.groupid
@@ -15,12 +18,14 @@ class TestAbrechnungsBot(unittest.TestCase):
         self.bot_mock = Mock()
 
         self.bot.start(Mock(), self.update_mock)
-        self.group = self.groups[self.groupid]
+
+    def _group(self):
+        return self.billingdata.groups[self.groupid]
     
     def test_add_acc(self):
         self.bot.add_account(self.bot_mock, self.update_mock, ["Ich"])
 
-        self.assertEqual("Ich", self.group.accounts[0].name)
+        self.assertEqual("Ich", self._group().accounts[0].name)
         
     def test_add_evt(self):
         self.bot.add_event(self.bot_mock, self.update_mock, [100, "Du", "Ich"])
